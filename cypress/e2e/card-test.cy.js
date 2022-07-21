d47a917d133337e6e9dc63b26cc14ba48e6c3975
NeilvB <neilvanbeinum@gmail.com>
2022-07-21 17:01:00 +0300

    WIP card test

--- cypress/e2e/card-test.cy.js
+++ cypress/e2e/card-test.cy.js
@@ -1,12 +1,12 @@
 describe('Card Test', () => {
-  xit('Cannot start a test if no cards have been made', () => {
+  it('Cannot start a test if no cards have been made', () => {
     cy.visit('/')
 
     cy.contains('button', 'Start Test').should('be.disabled')
   })
   
-  it('Presents a test with all cards in random order', () => {
-    cy.loadCardsIntoStorage([
+  it('Presents a test with all cards', () => {
+    const cards = [
       {
         frontText: 'front1',
         backText: 'back1'
@@ -15,12 +15,37 @@ describe('Card Test', () => {
         frontText: 'front2',
         backText: 'back2'
       }
-    ])
+    ]
+
+    cy.loadCardsIntoStorage(cards)
     
     cy.visit('/')
-    
+
     cy.contains('button', 'Start Test').click()
 
-    cy.get('div#card-test').contains('')
+    cy.get('fieldset#create-card-fieldset').should('be.disabled')
+    
+    cy.get('div#test-container').contains('front1')
+    
+    cy.contains('button', 'Flip Card').click()
+    cy.get('div#test-container').contains('back1')
+    
+    cy.contains('button', 'Flip Card').click()
+    cy.get('div#test-container').contains('front1')
+
+    cy.contains('button', 'Next Card').click()
+
+    cy.get('div#test-container').contains('front2')
+    
+    cy.contains('button', 'Flip Card').click()
+    cy.get('div#test-container').contains('back2')
+
+    cy.contains('button', 'Next Card').should('be.disabled')
+
+    cy.contains('button', 'End Test').click()
+    
+    cy.get('fieldset#create-card-fieldset').should('not.be.disabled')
+
+    cy.get('div#test-container').should('be.empty')
   })
 })
